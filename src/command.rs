8effaf41a9b17b9efbccc48f37136b8c563fc1b7
10maurycy10 <10maurycy10@users.noreply.github.com>
2020-11-05 11:50:35 -0800

    some refactoring

--- src/command.rs
+++ src/command.rs
@@ -3,6 +3,115 @@ use serde_yaml;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+
+/// ### print command help
+fn help() {
+    print!(
+"\
+help : displays available commands; world.aliases = ?
+go [north, south, west, east, up, down] : move in a direction; aliases = n s e w u d
+take [objects] : take an objects; world.aliases = t [objects]
+drop [object] : drop an object form 
+look [objects] : look at an object's you ; aliases l
+inventory <objects> : look at you backpack ; aliases i
+save [file] : save game data to json
+load [file] : load game data from json
+time : you sit arround ; aliases = .
+"   );
+}
+
+fn save(input :&mut dyn Iterator<Item = String>, src: &World) {
+    match input.next() {
+        Some(path) => {
+            match File::create(path.clone()) {
+                Err(why) => println!("couldn't open {}: {}", path.clone(), why),
+                Ok(mut file) => file
+                   .write_all(serde_yaml::to_string(src).unwrap().as_bytes())
+                   .unwrap(),
+                };
+            }
+        None => println!("You must specify a file path."),
+    };
+}
+
+fn load(input :&mut dyn Iterator<Item = String>) -> Option<World> {
+    match input.next() {
+        Some(path) => {
+            match fs::read_to_string(path.clone()) {
+                Ok(x) => {
+                    let deseralized: World = serde_yaml::from_str(&x).unwrap();
+                    return Some(deseralized);
+                }
+                Err(x) => println!("couldn't open {}: {}", path.clone(), x),
+            };
+        },
+        None => println!("please specify the file name"),
+    };
+    None
+}
+
+fn goto(input :&mut dyn Iterator<Item = String>, room :&Place) -> Option<String> {
+    match input.next() {
+        Some(dir) => match room.moves.get(&dir) {
+            Some(dest) => Some(dest.to_string()),
+            None => {
+                println!("You can't go that way.");
+                None
+            },
+        },
+        None => {
+            println!("You must specify a direction.");
+            None
+        },
+    }
+}
+
+fn take(room: &mut Place, ids : Vec<usize> , player :&mut LazzyCritter) {
+    if room.objects[ids[0]].can_take {
+        println!("you take the {}", room.objects[ids[0]].f());
+        let mut c = player.unpack();
+        c.backpack.push(room.objects[ids[0]].clone());
+        player.mutate(c);
+        room.objects.remove(ids[0]);
+    } else {
+        println!("nice try but...");
+    }
+}
+
+fn drop(room: &mut Place, ids : Vec<usize> , player :&mut LazzyCritter) {
+    let mut c = player.unpack();
+    if c.backpack[ids[0]].can_take {
+        println!("you drop the {}", c.backpack[ids[0]].f());
+        room.objects.push(c.backpack[ids[0]].clone());
+        c.backpack.remove(ids[0]);
+    } else {
+        println!("nice try but...");
+    }
+    player.mutate(c);
+}
+
+/// attack a target
+fn attack(attack :&Attack,target: Critter,room: &mut Place) -> Option<Critter> {
+    let mut r = target;
+    println!(
+        "you {} {}",
+        attack.name.p,
+        r.desc
+    );
+    r.hurt(attack.dam);
+    if !r.is_dead() {
+        println!("{}", r.hurt);
+    } else {
+        println!(
+            "you kill {}",
+            r.desc
+        );
+        r.kill(room);
+        return None;
+    };
+    Some(r)
+}
+
 /// # todo REFACTOR THIS!
 /// ### pharse command and do it.
 pub fn command(mut input_str: String, world: &mut World, game_over: &mut bool) {
@@ -36,68 +145,30 @@ pub fn command(mut input_str: String, world: &mut World, game_over: &mut bool) {
         }
         None => (),
     };
-    let curent_room = match world.map.get_mut(&world.player.location) {
+    let mut curent_room = match world.map.get_mut(&world.player.location) {
         Some(x) => x,
         None => panic!("Room {:?} is not in map", world.player.location),
     };
     let mut input_iter = input.into_iter();
     let start = input_iter.next().unwrap();
     match &*start {
-        "help" => {
-            print!(
-                "\
-help : displays available commands; world.aliases = ?
-go [north, south, west, east, up, down] : move in a direction; aliases = n s e w u d
-take [objects] : take an objects; world.aliases = t [objects]
-drop [object] : drop an object form 
-look [objects] : look at an object's you ; aliases l
-inventory <objects> : look at you backpack ; aliases i
-save [file] : save game data to json
-load [file] : load game data from json
-time : you sit arround ; aliases = .
-"
-            );
-        }
-        "save" => match input_iter.next() {
-            Some(path) => {
-                match File::create(path.clone()) {
-                    Err(why) => println!("couldn't open {}: {}", path.clone(), why),
-                    Ok(mut file) => file
-                        .write_all(serde_yaml::to_string(world).unwrap().as_bytes())
-                        .unwrap(),
-                };
-            }
-            None => println!("You must specify a file path."),
-                    },
-        "load" => match input_iter.next() {
-            Some(path) => {
-                match fs::read_to_string(path.clone()) {
-                    Ok(x) => {
-                        let deseralized: World = serde_yaml::from_str(&x).unwrap();
-                        *world = deseralized;
-                        redisplay = true;
-                    }
-                    Err(x) => println!("couldn't open {}: {}", path.clone(), x),
-                };
-            }
-            None => println!("You must specify a file path."),
+        "help" => help(),
+        "save" => save(&mut input_iter, &world),
+        "load" => match load(&mut input_iter) {
+            Some(x) => *world = x,
+            None => ()
         },
-        "exit" => {
-            *game_over = true;
-        }
+        "exit" => *game_over = true,
         "go" => {
-            match input_iter.next() {
-                Some(dir) => match curent_room.moves.get(&dir) {
-                    Some(dest) => world.player.location = dest.to_string(),
-                    None => println!("You can't go that way."),
-                },
-                None => println!("You must specify a direction."),
-            }
+            match goto(&mut input_iter,&curent_room) {
+                Some(x) => world.player.location = x,
+                None => ()
+            };
             redisplay = true;
-        }
+        },
         "look" => {
             redisplay = true;
-        }
+        },
         "take" => {
             match get_name(
                 &curent_room
@@ -107,17 +178,7 @@ time : you sit arround ; aliases = .
                     .collect(),
                 input_iter.collect(),
             ) {
-                NameResolves::Results(ids) => {
-                    if curent_room.objects[ids[0]].can_take {
-                        println!("you take the {}", curent_room.objects[ids[0]].f());
-                        let mut c = world.player.critter.unpack();
-                        c.backpack.push(curent_room.objects[ids[0]].clone());
-                        world.player.critter.mutate(c);
-                        curent_room.objects.remove(ids[0]);
-                    } else {
-                        println!("nice try but...");
-                    }
-                }
+                NameResolves::Results(ids) => take(curent_room,ids,&mut world.player.critter),
                 NameResolves::EmptyQuery => println!("You must specify a thing."),
                 NameResolves::Zero => println!("You can't find that."),
             }
@@ -129,21 +190,12 @@ time : you sit arround ; aliases = .
             }
         }
         "drop" => {
-            let mut c = world.player.critter.unpack();
+            let c = world.player.critter.unpack();
             match get_name(
                 &c.backpack.iter().map(|x| x.names.clone()).collect(),
                 input_iter.collect(),
             ) {
-                NameResolves::Results(ids) => {
-                    if c.backpack[ids[0]].can_take {
-                        println!("you drop the {}", c.backpack[ids[0]].f());
-                        curent_room.objects.push(c.backpack[ids[0]].clone());
-                        
-                        c.backpack.remove(ids[0]);
-                    } else {
-                        println!("nice try but...");
-                    }
-                }
+                NameResolves::Results(ids) => drop(&mut curent_room,ids,&mut world.player.critter),
                 NameResolves::EmptyQuery => println!("You must specify a thing."),
                 NameResolves::Zero => println!("You can't find that."),
             };
@@ -158,7 +210,7 @@ time : you sit arround ; aliases = .
             ) {
                 NameResolves::Results(ids) => {
                     if c.backpack[ids[0]].can_take {
-                        match (c.backpack[ids[0]].food.clone()) {
+                        match c.backpack[ids[0]].food.clone() {
                             Some(x) => {
                                 println!("you eat {} for {}", c.backpack[ids[0]].desc, x);
                                 c.hp = min(c.max_hp, c.hp + x);
@@ -206,26 +258,11 @@ time : you sit arround ; aliases = .
                         },
                         NameResolves::Results(ids) => {
                             let target_id = ids[0];
-                            let mut target = curent_room.critters[target_id].unpack();
-                            println!(
-                                "you {} {}",
-                                c.attack.name.p,
-                                target.desc
-                            );
-                            target.hurt(c.attack.dam);
-                            if !target.is_dead() {    
-                                println!("{}", target.hurt);
-                            } else {
-                                println!(
-                                    "you kill {}",
-                                    target.desc
-                                );
-                                target.kill(curent_room);
-                                curent_room.critters.remove(target_id);
+                            let target = curent_room.critters[target_id].unpack();
+                            match attack(&c.attack,target.clone(),curent_room) {
+                                Some(x) => curent_room.critters[target_id].mutate(x),
+                                None => {curent_room.critters.remove(target_id);},
                             };
-                            if (curent_room.critters.len() > target_id) {
-                                curent_room.critters[target_id].mutate(target);
-                            }
                         }   
                     };
                 },
@@ -252,7 +289,7 @@ time : you sit arround ; aliases = .
                         }
                         NameResolves::Results(target_ids) => {
                             let target_id = target_ids[0];
-                            let mut target = curent_room.critters[target_id].unpack();
+                            let target = curent_room.critters[target_id].unpack();
                             match get_name(
                                 &c.backpack.iter().map(|x| x.names.clone()).collect(),
                                 (*input[1]
@@ -273,23 +310,9 @@ time : you sit arround ; aliases = .
                                     let weppon = &c.backpack[weppon_ids[0]];
                                     match weppon.wepon {
                                         Some(ref data) => {
-                                            println!(
-                                                "you {} {} with {}",
-                                                data.name.p,
-                                                target.desc,
-                                                weppon.f()
-                                            );
-                                            target.hurt(data.dam);
-                                            if !target.is_dead() {
-                                                println!("{}", target.hurt);
-                                            } else {
-                                                println!(
-                                                    "you kill {} with {}",
-                                                    target.desc,
-                                                    weppon.f()
-                                                );
-                                                target.kill(curent_room);
-                                                curent_room.critters.remove(target_id);
+                                            match attack(&data,target.clone(),curent_room) {
+                                                Some(x) => curent_room.critters[target_id].mutate(x),
+                                                None => {curent_room.critters.remove(target_id);},
                                             }
                                         }
                                         None => {
@@ -299,9 +322,6 @@ time : you sit arround ; aliases = .
                                     };
                                 }
                             };
-                            if (curent_room.critters.len() > target_id) {
-                                curent_room.critters[target_id].mutate(target);
-                            }
                         }
                     };
                 }
